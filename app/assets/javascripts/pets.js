5c6497d4b6d22c8ea785898c0ad93616edfb2b35
cyzanfar <cghazanfar10@gmail.com>
2015-03-23 17:37:56 -0400

    modal

--- app/assets/javascripts/pets.js
+++ app/assets/javascripts/pets.js
@@ -1,8 +1,9 @@
 $(function(){
-	debugger;
+	// debugger;
 	favPetListener();
 	hideForm();
 	contactShelter();
+	hideModal();
 	likePet();
 	showForm();
 	hideCancel();
@@ -26,8 +27,23 @@ function showForm() {
 }
 
 function contactShelter() {
-	$('.contact-shelter').click(function(){
-		alert("Sign in to contact this shelter!");
+	// // $('.contact-shelter').click(function(){
+	// 	// alert("Sign in to contact this shelter!");
+	$('#myModal').on('shown.bs.modal', function () {
+		// debugger;
+	$('#myModal').show();
+
+	// $('#myInput').focus()
+	});
+
+	// $('.contact-shelter').click(function(){
+	// 	debugger;
+	// 	$('#myModal').show();
+	// });
+}
+function hideModal() {
+	$('.close').click(function(){
+		$('#myModal').hide();
 	});
 }
 function hideCancel() {
